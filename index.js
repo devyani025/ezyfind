88bab6252ad8d469c267a589f4649b833964ba2e
devyani025 <70661479+devyani025@users.noreply.github.com>
2021-04-24 11:02:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
- * @format
+ * import react-native packages required for Loader @class
  */
-import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import { ActivityIndicator, View} from 'react-native'
+import styles from './LoaderStyle';
 
-AppRegistry.registerComponent(appName, () => App);
+export default class Loader extends React.Component {  
+  render() {
+    return (
+      <View style={{justifyContent:'center',alignItems:'center',alignContent:'center',alignSelf:'center',flex:1}}>
+        <ActivityIndicator size="large" style={{ position: 'absolute', alignItems: 'center', justifyContent: 'center', }} color="#FE5665" />        
+      </View> 
+    )
+  }
+}
